e3c6135c15cd041bf86b2b24b492eed119afc04b
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-11-19 11:32:37 +0100

    feat: complete prime numbers of 7.11

--- Lab7/7.11/main.c
+++ Lab7/7.11/main.c
@@ -5,10 +5,14 @@
 
 bool czy_automorficzna(int liczba, int modulo);
 void znajdz_automorficzne(int n);
+bool czy_pierwsza(int liczba);
+void znajdz_pierwsze(int zakres);
 
 int main()
 {
+    printf("Liczby automorficzne: \n");
     znajdz_automorficzne(100000);
+    znajdz_pierwsze(200);
     return 0;
 }
 
@@ -31,4 +35,32 @@ void znajdz_automorficzne(int max){
             printf("%d x %d = %d\n", i, i, pow(i, 2));
         }
     }
+}
+
+bool czy_pierwsza(int liczba){
+    if(liczba < 2){
+        return false;
+    }
+
+    // Pętla od 2 do liczba/2
+    // Dla liczb 2 i 3 pętla nie wykona się ani razu, bo (liczba / 2 = 1)
+    for(int i = 2; i <= liczba / 2; i++){
+        // Jeżeli liczba jest podzielna przez i, to nie jest pierwsza
+        if(liczba % i == 0){
+            return false;
+        }
+    }
+
+    // Jeżeli funkcja nie zwróciła false w pętli to znaczy, że jest liczba jest liczbą pierwszą
+    return true;
+}
+
+void znajdz_pierwsze(int zakres){
+    printf("\nLiczby pierwsze: ");
+    for(int i = 2; i <= zakres; i++){
+        if(czy_pierwsza(i)){
+            printf("%d, ", i);
+        }
+    }
+    printf("\n");
 }
