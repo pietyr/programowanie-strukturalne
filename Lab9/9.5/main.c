a9853cef03b1a90cbc0a5e87899552b0882373bd
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-12-01 12:06:28 +0100

    feat: complete 9.5

---
+++ Lab9/9.5/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void dekompozycja(double liczba, int *czesc_calkowita, double *czesc_ulamkowa);
+
+int main() {
+    double liczba;
+    double czesc_ulamkowa;
+    int czesc_calkowita;
+
+    for(int i = 0; i < 3; i++){
+        printf("Podaj liczbę rzeczywistą: ");
+        scanf("%lf", &liczba);
+        dekompozycja(liczba, &czesc_calkowita, &czesc_ulamkowa);
+        printf("Część całkowita: %d, Część ułamkowa: %lf\n", czesc_calkowita, czesc_ulamkowa);
+    }
+    return 0;
+}
+
+void dekompozycja(double liczba, int *czesc_calkowita, double *czesc_ulamkowa){
+    *czesc_calkowita = (int)liczba;
+    *czesc_ulamkowa = liczba - ((double)*czesc_calkowita);
+}
